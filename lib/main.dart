23d61cdb73159a51936a65d2b3a5636ac40835dd
ishtiaq rashid <ishtiaqrashid299@gmail.com>
2023-07-14 14:48:15 +0500

    changes in panel screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:eziline_project/screens/panel_selection_screen.dart';
 import 'package:eziline_project/splash_screens/splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -8,7 +9,6 @@ void main() async{
 }
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
--- lib/screens/panel_selection_screen.dart
+++ lib/screens/panel_selection_screen.dart
@@ -1,3 +1,6 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:carousel_slider/carousel_controller.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:eziline_project/screens/login_screen.dart';
 import 'package:eziline_project/style/colors.dart';
 import 'package:eziline_project/style/textstyle.dart';
@@ -14,46 +17,153 @@ class panel_selection extends StatefulWidget {
 }
 
 class _panel_selectionState extends State<panel_selection> {
+
+
+  //for carouselSlider
+  int _current = 0;
+  final CarouselController _controller = CarouselController();
+  final List<String> imgList = [
+    "images/2.png",
+    "images/4.png",
+    "images/2.png",
+    "images/4.png",
+
+
+  ];
+
+  bool isLoading=false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColor.BG_COLOR,
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          SizedBox(height: 100,),
+          // SizedBox(height: 100,),
           Container(
-            height: 200,
-            width: double.infinity,
-            decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage("images/2.png")
-                )
+
+            padding: EdgeInsets.symmetric(horizontal: 30),
+            child: CarouselSlider(
+              options: CarouselOptions(
+                  autoPlayAnimationDuration: Duration(seconds: 1),
+                  autoPlayInterval: Duration(seconds: 2),
+                  viewportFraction: 1,
+                  height: 250,
+                  autoPlay: true,
+                  aspectRatio: 2.0,
+                  enlargeCenterPage: true,
+                  onPageChanged: (index, carouselReason) {
+                    print(index);
+                    setState(() {
+                      _current=index;
+                    });
+                  }
+              ),
+              items: imgList
+                  .map((item) => Container(
+                child: Container(
+                  margin: EdgeInsets.all(5.0),
+                  child: ClipRRect(
+                      borderRadius: BorderRadius.all(Radius.circular(12.0)),
+                      child: Stack(
+                        children: <Widget>[
+                          Image.asset(item, fit: BoxFit.cover, width: 1000.0),
+                        ],
+                      )),
+                ),
+              ))
+                  .toList(),
             ),
           ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: imgList.asMap().entries.map((entry) {
+              return GestureDetector(
+                onTap: () => _controller.animateToPage(entry.key),
+                child: Container(
+                  width: 40,
+                  height: 3.0,
+                  margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
+                  decoration: BoxDecoration(
+                      shape: BoxShape.rectangle,
+                      color: (Theme.of(context).brightness == Brightness.dark
+                          ? Colors.white
+                          : AppColor.APP_COLOR)
+                          .withOpacity(_current == entry.key ? 0.9 : 0.4)),
+                ),
+              );
+            }).toList(),
+          ),
+          SizedBox(height: 70,),
+          AnimatedTextKit(
+            animatedTexts: [
+              WavyAnimatedText('Welcome',textStyle: TextStyle(
+                fontWeight: FontWeight.w500,
+                fontSize: 22,
+                color: Colors.black54,
+              )),
+              WavyAnimatedText('Please Select Your Panel',textStyle: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.w500,
+                color: Colors.black54,
+              )),
+            ],
+            isRepeatingAnimation: true,
+            onTap: () {
+              print("Tap Event");
+            },
+          ),
           SizedBox(height: 50,),
-          Text("Welcome to Technology school\n         please select your panel",style: GoogleFonts.poppins(
-            textStyle: AppTextStyles.HEADING_TEXT,
-          ),),
-          SizedBox(height: 30,),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              button(
-                title: 'Student',
-                btnColor: AppColor.APP_COLOR,
+              MaterialButton(
+                splashColor: AppColor.APP_COLOR,
+                elevation: 5,
                 onPressed: (){
-                  Navigator.of(context).push(MaterialPageRoute(builder: (_)=>login_screen()));
+                Navigator.of(context).push(MaterialPageRoute(builder: (_)=>login_screen()));
                 },
-                width: 100,
-              ),
-              button(
-                title: 'Admin',
-                btnColor: AppColor.APP_COLOR,
+                height: 48,
+                 child:isLoading?CircularProgressIndicator(
+                color: Colors.white,
+                ):Text("Student",style: GoogleFonts.poppins(
+                textStyle: TextStyle(
+                  color: Colors.black54,
+                  fontWeight: FontWeight.w600,
+                  fontSize: 16,
+                )
+                ),),
+               minWidth: 100,
+                shape: RoundedRectangleBorder(
+              side: BorderSide(color: AppColor.APP_COLOR,width: 2),
+              borderRadius: BorderRadius.circular(12),
+               ),
+            color: Colors.white,
+          ),
+              MaterialButton(
+                splashColor: AppColor.APP_COLOR,
+                elevation: 5,
                 onPressed: (){
                   Navigator.of(context).push(MaterialPageRoute(builder: (_)=>admin_login()));
                 },
-                width: 100,
+                height: 48,
+                child:isLoading?CircularProgressIndicator(
+                  color: Colors.white,
+                ):Text("Admin",style: GoogleFonts.poppins(
+                    textStyle: TextStyle(
+                      color: Colors.black54,
+                      fontWeight: FontWeight.w600,
+                      fontSize: 16,
+                    )
+                ),),
+                minWidth: 100,
+                shape: RoundedRectangleBorder(
+                  side: BorderSide(color: AppColor.APP_COLOR,width: 2),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                color: Colors.white,
               ),
+
             ],
           ),
         ],
